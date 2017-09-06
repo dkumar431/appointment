ac1fcfe3b188f3a6a2b8d6f60cda0a3f9bb983eb
Tilak Yadav <tilak256@gmail.com>
2017-09-06 20:32:35 +0530

    Booking confirmation modal

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -1,2 +1,27 @@
 class BookingController < ApplicationController
+  before_action :set_patient, only: [:show, :edit, :update, :destroy]
+  
+  def new
+      @booking = Booking.new
+      @patient = Patient.new
+  end
+  def create 
+    @patient = Patient.new(patient_params)
+    @patient.save
+    @booking = Booking.find(params[:booking_id])
+    @booking.patient_id = @patient.id
+    @booking.save
+  end
+
+  def show
+  end
+  private
+  def set_patient
+    @patient = Patient.find(params[:id])
+  end
+
+  def patient_params
+    params.permit(:name, :mobile, :email, :ssn)
+  end
+
 end
