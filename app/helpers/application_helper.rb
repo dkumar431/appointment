dabb62ff69f5f83d55189a4480e50253f56bc736
vramisetti <vramisetti@partner.aligntech.com>
2017-09-07 00:06:54 +0530

    tabs display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def get_url date
+        url = URI.encode("#{request.base_url}#{request.path}?term=#{params[:term]}&doctor_id=#{params[:doctor_id]}&clinic=#{params[:clinic]}&date=#{date}")
+    end
 end
