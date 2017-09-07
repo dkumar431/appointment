81cdcff3cb6ecf50bd23d26d44916521fc76608d
Deepak Kumar <dpadhy@partner.aligntech.com>
2017-09-07 16:48:31 +0530

    address fields

---
+++ db/migrate/20170907104022_add_address_id_to_clinic.rb
@@ -0,0 +1,14 @@
+class AddAddressIdToClinic < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :clinics, :address_line1
+    remove_column :clinics, :address_line2
+    remove_column :clinics, :city
+    remove_column :clinics, :state
+    remove_column :clinics, :zip
+    remove_column :clinics, :longitude
+    remove_column :clinics, :latitude
+    add_reference :clinics, :address, index: true
+    add_column :addresses, :lat, :decimal, {:precision=>10, :scale=>6}
+    add_column :addresses, :lng, :decimal, {:precision=>10, :scale=>6}
+  end
+end
